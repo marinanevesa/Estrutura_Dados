5ee9dcafe521a9d48f76c1e85167cdd53e18437f
Marina Neves <marinanevesa@gmail.com>
2026-03-05 21:47:00 -0300

    21_46

---
+++ Deque.mjs
@@ -0,0 +1,7 @@
+//double-ended Queue (fila de duas pontas)
+//posso colocar no comeco ou no fim da fila e mexer dos dois lados
+//nao mexo no mei
+
+//nao mexo no meio da estrutura, ou seja, nao posso acessar um elemento do meio da estrutura, apenas o primeiro e o ultimo elemento
+
+//nao tem indice, ou seja, nao posso acessar um elemento pelo indice, apenas o primeiro e o ultimo elemento
