2f6a0705db317085f386ca0c7d3972be1db30ba3
Marina Neves <marinanevesa@gmail.com>
2026-03-26 20:44:17 -0300

    25_marco

---
+++ Lista_dupla_encadiada.mjs
@@ -0,0 +1,4 @@
+// cada nodo da lista encadeade tem tres partes: uma , que armazena a inf, a segunda q guarda o endeço do nodo anterior, e a terceira, que guarda o endereço do nodo posterior
+// no com 3 infos - valor, endereço do nodo anterior, endereço do nodo posterior
+// mais rapida, consigo maxer no meio
+//
